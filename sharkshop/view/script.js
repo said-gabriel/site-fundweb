88790e80aded28b6850ea373589617c2d43dd6d8
Xavier <lucas.xavier@sensedia.com>
2022-08-01 19:19:46 -0300

    merge 01/08

---
+++ sharkshop/view/script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  /*
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}*/
